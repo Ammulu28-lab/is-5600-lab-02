a63a4a2f5c879415d907d4e5cb1041aa930a347f
Ammulu28-lab <manasa.paleru@slu.edu>
2024-11-05 04:24:25 +0000

    started the document

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
-/* add your code here */
+document.addEventListener('DOMContentLoaded', () => {
+    const stocksData = JSON.parse(stockContent);
+    const userData = JSON.parse(userContent);
+    console.log({userData});
+  });
 
